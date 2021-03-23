6ece797133ff9ee99dae8a402597e94ee47a3194
SilMontes <Silmontes2001@gmail.com>
2021-03-23 20:57:41 +0000

    Done

--- exercises/18-Random-Colors-Loop/app.py
+++ exercises/18-Random-Colors-Loop/app.py
@@ -19,6 +19,11 @@ def get_allStudentColors():
     example_color = 1
     students_array = []
     #your loop here
+    for i in range(10):
+        random_number=random.randint(0,4)
+        color=get_color(random_number)
+        students_array.append(color)
+    return students_array
 
 
 
